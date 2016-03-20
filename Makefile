13c614ec85774751f88c7250a67896e2658654e0
Jocelyn Bohr <bjocelyn@umich.edu>
2016-03-20 14:06:42 -0400

    Added svm.cc. very messy

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 CC = g++
 
 OPENCV_LIBS = -L/usr/local/lib -lopencv_shape -lopencv_stitching -lopencv_objdetect -lopencv_superres -lopencv_videostab -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo -lopencv_ml -lopencv_imgproc -lopencv_flann -lopencv_core -lopencv_hal
-LIBS = $(OPENCV_LIBS)
+BOOST_LIBS = -lboost_system -lboost_filesystem
+LIBS = $(BOOST_LIBS) $(OPENCV_LIBS) -lm -ldl
 OPENCV_FLAGS = -I/usr/local/include/opencv -I/usr/local/include
 CFLAGS = -g -Wall -Werror -std=c++11 $(OPENCV_FLAGS)
 
-default: test
+default: svm
 
-svm: svm.cc
-	$(CC) $(CFLAGS) svm.cc -o $@ $(LIBS)
+svm: svm.cc preprocess.cc CKTrainData.cc
+	$(CC) $(CFLAGS) svm.cc preprocess.cc CKTrainData.cc -std=c++11 -o $@ $(LIBS)
 
 preprocess.o: preprocess.cc preprocess.h
 	$(CC) $(CFLAGS) preprocess.cc -c $(LIBS)
@@ -17,7 +18,7 @@ test: test.cc preprocess.o
 	$(CC) $(CFLAGS) test.cc preprocess.o -o $@ $(LIBS)
 
 filesystem: filesystem.cc CKTrainData.cc
-	$(CC) filesystem.cc CKTrainData.cc -std=c++11 -o $@ -lboost_system -lboost_filesystem
+	$(CC) filesystem.cc CKTrainData.cc -std=c++11 -o $@ $(BOOST_LIBS)
 
 clean:
 	rm -rf *.o svm test
--- svm.cc
+++ svm.cc
@@ -2,31 +2,85 @@
 #include <iostream>
 #include <vector>
 
+#include "CKTrainData.h"
+#include "preprocess.h"
+
 using namespace cv;
 using namespace ml;
-using namespace fs = boost::filesystem
+using namespace std;
 
 int main() {
 
   int height = 512, width = 512;
   Mat image = Mat::zeros(height, width, CV_8UC3);
 
-  // set up training data
+  CKTrainData ckdata;
+  ckdata.init();
+  vector<Mat> train_x;
+  vector<int> train_t;
+  vector<Mat> test_x;
+  vector<int> test_t;
+  Mat m;
+
+  for (unsigned int i = 0; i < ckdata.filenames.size(); ++i) {
+    for (unsigned int j = 0; j < ckdata.filenames[i].size(); ++j) {
+      if (ckdata.labels[i][j] == -1) continue;
+      if (preprocess(ckdata.filenames[i][j][0], m) != 0) {
+        cout << "preprocess failed: " << ckdata.filenames[i][j][0] << endl;
+        exit(1);
+      }
+      unsigned int end = ckdata.filenames[i][j].size();
+      // test generalization to new subjects
+      if (i % 9) {
+        train_x.push_back(m);
+        train_t.push_back(ckdata.labels[i][j]);
+      } else {
+        test_x.push_back(m);
+        test_t.push_back(ckdata.labels[i][j]);
+      }
+
+      if (preprocess(ckdata.filenames[i][j][end-1], m) != 0) {
+        cout << "preprocess failed: " << ckdata.filenames[i][j][end-1] << endl;
+        exit(1);
+      }
+      // test generalization to new subjects
+      if (i % 9) {
+        train_x.push_back(m);
+        train_t.push_back(ckdata.labels[i][j]);
+      } else {
+        test_x.push_back(m);
+        test_t.push_back(ckdata.labels[i][j]);
+      }
+    }
+  }
+
+/*  // set up training data
+  // TODO: replace dummy data with cohn-kanade data. get filenames, preprocess,
+  // extract gabor features
   int labels[4] = {1, -1, -1, -1};
   Mat labelsMat(4, 1, CV_32S, labels);
   float trainingData[4][2] = { {501, 10}, {255, 10}, {501, 255}, {10, 501} };
-  Mat trainingDataMat(4, 2, CV_32FC1, trainingData);
+  Mat trainingDataMat(4, 2, CV_32FC1, trainingData);*/
 
   // train svm
   Ptr<SVM> svm = SVM::create();
-  svm->setType(SVM::C_SVC);
+  svm->trainAuto(TrainData::create(train_x, ROW_SAMPLE, train_t));
+/*  svm->setType(SVM::C_SVC);
   svm->setKernel(SVM::LINEAR);
   svm->setGamma(3);
 
-  svm->train(trainingDataMat, ROW_SAMPLE, labelsMat);
-  Vec3b green (0,255,0), blue (255,0,0);
-  // show decision regions given by svm
+  svm->train(train_x, ROW_SAMPLE, train_t);*/
 
+  int correct = 0;
+  for (unsigned int i = 0; i < test_x.size(); ++i) {
+    int response = svm->predict(test_x[i]);
+    if (response == test_t[i]) ++correct;
+  }
+  double accuracy = (double)correct/(double)test_x.size();
+
+  cout << "accuracy : " << accuracy << endl;
+/*
+  // show decision regions given by svm
   for (int i = 0; i < image.rows; ++i) {
     for (int j = 0; j < image.cols; ++j) {
       float sample[2] = {(float)j, (float)i};
@@ -37,8 +91,8 @@ int main() {
       else if ((int)response == -1) image.at<Vec3b>(i,j) = blue;
     }
   }
-
-  // show training data
+*/
+/*  // show training data
   int thickness = -1;
   int lineType = 8;
   circle( image, Point(501,  10), 5, Scalar(  0,   0,   0), thickness, lineType );
@@ -57,5 +111,5 @@ int main() {
   }
   imwrite("result.png", image);
   imshow("SVM simple example", image);
-  waitKey(0);
+  waitKey(0);*/
 }
