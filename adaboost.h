6c85574cc449a49db21de7a07deea49b37779658
johnconn <johnconn@umich.edu>
2016-04-02 05:05:00 -0400

    gabor visualization of adaboost chosen features

--- adaboost.h
+++ adaboost.h
@@ -12,8 +12,8 @@ class Adaboost {
   private:
     cv::Mat data; // Rows - samples, columns - features
     std::vector<cv::Mat> labels; // Index i is Mat of binary labels for emotion i
-    std::set<int> weak_learners_indices;
   public:
+    std::set<int> weak_learners_indices;
     Adaboost(cv::Mat data_, cv::Mat labels_, unsigned int weak_learners = 82,
         bool retrain = true);
     cv::Mat reduce_features(const cv::Mat &original_mat);
